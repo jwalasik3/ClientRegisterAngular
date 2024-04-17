805b245c811e9f4cea520617554b79c88750504f
jwalasik3 <walasikjakub@gmail.com>
2024-04-17 22:04:21 +0200

    help me with CR

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  exports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
 })
 export class SharedModule {}
